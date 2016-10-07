9a5273034071960c004eda5d6ca23e05195039ab
rukshan <ruky68@live.com>
2016-10-07 13:37:45 +0530

    combine with helpdesk app

---
+++ helpdesk-app/src/main/java/org/wso2/sample/loyalty/APIClient.java
@@ -0,0 +1,16 @@
+package org.wso2.sample.loyalty;
+
+/**
+ * Client for the loyalty API
+ */
+public class APIClient {
+
+    public void getAccessToken(String jwt){
+
+
+
+    }
+
+
+
+}
---
+++ loyalty-app/src/main/java/org/wso2/sample/loyalty/JWTDecoder.java
@@ -0,0 +1,7 @@
+package org.wso2.sample.loyalty;
+
+/**
+ * Created by rushmin on 10/6/16.
+ */
+public class JWTDecoder {
+}
